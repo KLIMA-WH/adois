c1dee6c316a727ef7b3dfde14ab12743ebc525b2
mrsmrynk <mrsmrynk@gmail.com>
2022-04-06 10:57:22 +0200

    feat: add delete_list for preprocess_mask_shp in utils

--- src/data/package/utils.py
+++ src/data/package/utils.py
@@ -4,12 +4,14 @@ import os
 
 def preprocess_mask_shp(path,
                         column,
-                        replacement_dict):
+                        replacement_dict,
+                        delete_list=None):
     """Preprocesses the shape file, so it can be used with MaskGenerator.
 
     :param str path: relative path to the shape file
     :param str column: name of the column of the class values
     :param dict replacement_dict: dictionary of each class value (key) and their mask value (value)
+    :param list of str or None delete_list: list of class values to delete
     :returns: None
     :rtype: None
     :raises ValueError: if value in replacement_dict is not valid (not a value between 0 and 255)
@@ -23,4 +25,8 @@ def preprocess_mask_shp(path,
     shapes = gpd.read_file(path)
     shapes['mask_value'] = shapes[column]
     shapes.replace({'mask_value': replacement_dict}, inplace=True)
+
+    if delete_list is not None:
+        shapes = shapes[~shapes['mask_value'].isin(delete_list)]
+
     shapes.to_file(os.path.join(os.path.dirname(path), f"{os.path.splitext(path)[0].split('/')[-1]}_preprocessed.shp"))
