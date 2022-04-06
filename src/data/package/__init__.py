ea0eb1c28d6e4db9cffc4a01dce1c92e2f924e96
mrsmrynk <mrsmrynk@gmail.com>
2022-04-06 13:22:21 +0200

    refactor: edit project structure

--- src/data/package/__init__.py
+++ src/data/package/__init__.py
@@ -1,4 +1,3 @@
 from src.data.package.mask_generator import *
-from src.data.package.record_generator import *
 from src.data.package.tile_generator import *
 from src.data.package.utils import *
---
+++ src/features/package/__init__.py
@@ -0,0 +1,1 @@
+from src.features.package.record_generator import *
