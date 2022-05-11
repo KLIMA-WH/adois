5b9bf5f418888fdd600af3bcf35394d35a2fc7dd
mrsmrynk <mrsmrynk@gmail.com>
2022-05-11 16:26:56 +0200

    style: reformat mask_generator

--- src/data/package/mask_generator.py
+++ src/data/package/mask_generator.py
@@ -19,7 +19,7 @@ logger_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(
 
 log_dir_path = Path(__file__).parents[1] / 'logs'
 date_time = str(DateTime.now().isoformat(sep='_', timespec='seconds')).replace(':', '-')
-file_handler = logging.FileHandler(filename=log_dir_path / f'{date_time}_mask_generator.log', mode='w')
+file_handler = logging.FileHandler(log_dir_path / f'{date_time}_mask_generator.log', mode='w')
 file_handler.setFormatter(logger_formatter)
 logger.addHandler(file_handler)
 
@@ -63,13 +63,13 @@ class MaskGenerator:
         if shp_path is not None:
             shp_path = Path(shp_path)
             shapes = gpd.read_file(shp_path)
-            self.shapes = list((row.geometry for _, row in shapes.iterrows()))
+            self.shapes = [row.geometry for _, row in shapes.iterrows()]
         else:
             self.shapes = None
         self.multi_class_mask = multi_class_mask
         self.create_wld = create_wld
         self.create_geotiff = create_geotiff
-        with open(self.metadata_path, 'r') as file:
+        with open(self.metadata_path, mode='r') as file:
             self.metadata = json.load(file)
         self.epsg_code = self.metadata.get('epsg code')
         self.resolution = self.metadata.get('resolution')
@@ -91,11 +91,12 @@ class MaskGenerator:
         bounding_box = utils.get_bounding_box(coordinates=coordinates,
                                               image_size_meters=self.image_size_meters)
 
-        shapes = gpd.read_file(self.mask_shp_path, bbox=bounding_box)
+        shapes = gpd.read_file(self.mask_shp_path,
+                               bbox=bounding_box)
         if self.multi_class_mask:
-            shapes = list((row.geometry, row.mask_value) for _, row in shapes.iterrows())
+            shapes = [(row.geometry, row.mask_value) for _, row in shapes.iterrows()]
         else:
-            shapes = list((row.geometry, MaskGenerator.SINGLE_CLASS_MASK_VALUE) for _, row in shapes.iterrows())
+            shapes = [(row.geometry, MaskGenerator.SINGLE_CLASS_MASK_VALUE) for _, row in shapes.iterrows()]
 
         transform = rio.transform.from_origin(west=coordinates[0],
                                               north=coordinates[1],
@@ -106,20 +107,22 @@ class MaskGenerator:
                                           out_shape=(self.image_size, self.image_size),
                                           transform=transform)
         except ValueError:
-            mask = np.zeros(shape=(self.image_size, self.image_size), dtype=np.uint8)
-        mask = np.moveaxis(np.expand_dims(mask, axis=-1), source=-1, destination=0)
+            mask = np.zeros(shape=(self.image_size, self.image_size),
+                            dtype=np.uint8)
+        mask = np.moveaxis(np.expand_dims(mask, axis=-1),
+                           source=-1,
+                           destination=0)
 
         if self.shapes is not None:
             with rio.io.MemoryFile() as memory_file:
-                with memory_file.open(
-                        driver='GTiff',
-                        width=self.image_size,
-                        height=self.image_size,
-                        count=MaskGenerator.BANDS,
-                        crs=f'epsg:{self.epsg_code}',
-                        transform=transform,
-                        dtype=mask.dtype,
-                        nodata=0) as dataset:
+                with memory_file.open(driver='GTiff',
+                                      width=self.image_size,
+                                      height=self.image_size,
+                                      count=MaskGenerator.BANDS,
+                                      crs=f'epsg:{self.epsg_code}',
+                                      transform=transform,
+                                      dtype=mask.dtype,
+                                      nodata=0) as dataset:
                     dataset.write(mask)
                 with memory_file.open() as dataset:
                     mask_masked, _ = rio.mask.mask(dataset=dataset,
@@ -148,7 +151,8 @@ class MaskGenerator:
                                                   north=coordinates[1],
                                                   xsize=self.resolution,
                                                   ysize=self.resolution)
-            with rio.open(path, 'w',
+            with rio.open(path,
+                          mode='w',
                           driver='GTiff',
                           width=self.image_size,
                           height=self.image_size,
@@ -160,10 +164,12 @@ class MaskGenerator:
                 file.write(image)
         else:
             # noinspection PyTypeChecker
-            Image.fromarray(np.moveaxis(image, source=0, destination=-1)).save(path)
+            Image.fromarray(np.moveaxis(image,
+                                        source=0,
+                                        destination=-1)).save(path)
 
         if self.create_wld:
-            utils.export_wld(path=path.with_suffix('.wld'),
+            utils.export_wld(path.with_suffix('.wld'),
                              resolution=self.resolution,
                              coordinates=coordinates)
 
@@ -184,17 +190,17 @@ class MaskGenerator:
         logger_padding_length = len(str(len(tiles_dir_file_list)))
 
         for index, file in enumerate(tiles_dir_file_list):
-            mask, coordinates = self.get_mask(path=tiles_dir_path / file)
+            mask, coordinates = self.get_mask(tiles_dir_path / file)
             image_name = Path(file).stem
             mask_name = f"mask_{'_'.join(image_name.split('_')[-3:])}.tiff"
-            self.export_mask(image=mask,
+            self.export_mask(mask,
                              path=self.dir_path / 'mask' / mask_name,
                              coordinates=coordinates)
             logger.info(f'iteration {index + 1:>{logger_padding_length}} / {iterations} '
                         f'-> mask with id = {index} exported')
 
         end_time = DateTime.now()
-        delta = utils.chop_microseconds(delta=end_time - start_time)
+        delta = utils.chop_microseconds(end_time - start_time)
 
         metadata = {'timestamp': str(DateTime.now().isoformat(sep=' ', timespec='seconds')),
                     'execution time': str(delta),
@@ -206,7 +212,7 @@ class MaskGenerator:
                     'number of columns': self.metadata.get('number of columns'),
                     'number of rows': self.metadata.get('number of rows'),
                     'number of iterations': self.metadata.get('number of iterations')}
-        utils.export_metadata(path=self.dir_path / 'mask_metadata.json',
+        utils.export_metadata(self.dir_path / 'mask_metadata.json',
                               metadata=metadata)
 
     @staticmethod
